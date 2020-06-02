9c894db66eb11f7a0a90dccbf2f278fcf9fabac2
Sayeed-001 <a.sayeed2521@gmail.com>
2020-06-02 20:05:17 +0530

    Step-1: Authenticating the website using uname and password for Login using Selenium webdriver

--- Scrapy Framework using Selenium Automating browser/paramaters.py
+++ Scrapy Framework using Selenium Automating browser/paramaters.py
@@ -1,6 +1,6 @@
 # PARAMATERS.PY used in SCRIPT.PY FILE
-chromedriver_path = 'C:/Users/Sayee/selenium_internshala/Initialize_Login_website/chromedriver'  # INSERT PATH OF YOUR CHROMEDRIVER
+chromedriver_path = '#'        # INSERT PATH OF YOUR CHROMEDRIVER
 base_url = 'https://internshala.com'   						  # Base URL
 
-internshala_user = 'a.sayeed2521@gmail.com'  				              # Insert Internshala Username
-user_password = 'America@123' 		 						  # Insert Internshala Password
+internshala_user = '#'  				              # Insert Internshala Username
+user_password = '#' 		 						  # Insert Internshala Password
--- Scrapy Framework using Selenium Automating browser/script.py
+++ Scrapy Framework using Selenium Automating browser/script.py
@@ -6,26 +6,36 @@ import paramaters
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(executable_path=paramaters.chromedriver_path)
-driver.maximize_window()
+# configure Chrome Webdriver
+# Add additional Options to the webdriver
+chrome_options = Options()
+# add the argument and make the browser Headless.
+#chrome_options.headless =True
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--disable-extensions")
+
+driver = webdriver.Chrome(executable_path=paramaters.chromedriver_path, options=chrome_options)
+driver.implicitly_wait(6)
 driver.get(paramaters.base_url)
 sleep(5)
 
 # Login tab click
 login_button = driver.find_element_by_xpath('//*[@class="nav-item"]/button')
 login_button.click()
-sleep(1)
+sleep(2)
 
 # Enter Username/Email ID
 user_email = driver.find_element_by_id('modal_email')
 user_email.send_keys(paramaters.internshala_user)
-sleep(1)
+sleep(2)
 
 # Enter user password
 user_password = driver.find_element_by_id('modal_password')
 user_password.send_keys(paramaters.user_password)
-sleep(1)
+sleep(2)
 
 # Sign in click
 sign_in_button = driver.find_element_by_id('modal_login_submit')
